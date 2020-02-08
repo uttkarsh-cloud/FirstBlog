44ee0269d7a89dabf0d9d5fe3433ce8fca2d410b
uttkarsh-cloud <ubajpai28@gmail.com>
2020-02-09 02:37:44 +0530

    changes in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xk+er8vrlxxjr=3(_(xl1$&8%cp$36sgf0#y#b_0-+xu7z=c6c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
