9f0ab32579037e3f1db91eb16e4f82039a6909d6
uttkarsh-cloud <ubajpai28@gmail.com>
2020-02-10 02:41:20 +0530

    security, publish and edits are added

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xk+er8vrlxxjr=3(_(xl1$&8%cp$36sgf0#y#b_0-+xu7z=c6c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 INSTALLED_APPS = [
@@ -116,3 +116,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+LOGIN_REDIRECT_URL = '/'
